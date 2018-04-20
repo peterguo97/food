52450c42c9b579e33be07a217d96ffc4755df79c
gjj <1623144737@qq.com>
2018-04-20 19:50:28 +0800

    index

--- src/components/ShopList.js
+++ src/components/ShopList.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { List } from 'antd-mobile';
+import { Link } from 'dva/router';
+import { List, WingBlank } from 'antd-mobile';
 import img from '../assets/food5.jpg';
 import normal from './css/basic.css';
 
 const Item = List.Item;
 const Brief = Item.Brief;
+const data = [{id: '1',name: '点点饲料'},{id:'2',name:'点点最爱'}];
 
 class ShopList extends React.Component {
     state = {
@@ -12,26 +14,31 @@ class ShopList extends React.Component {
     }
 
     render() {
-        return (<div>
-            <List renderHeader={() => '商家列表'} className={normal.list}>
-                <Item 
-                    arrow="horizontal"
-                    multipleLine
-                    onClick={() => { }}
-                    thumb={img}
-                    >
-                    宠物饲料 <Brief>点点饲料</Brief>
-                </Item>
-                <Item
-                    arrow="horizontal"
-                    multipleLine
-                    onClick={() => { }}
-                    thumb={img}
-                >
-                    宠物饲料 <Brief>点点饲料</Brief>
-                </Item>
-            </List>
-        </div>);
+        return (
+            <WingBlank>
+                <List renderHeader={() => '商家列表'} className={normal.list}>
+                    {
+                        data.map(function(val) {
+                            const str = val.id + val.name;
+                            console.log(str);
+                            return (
+                                <Item 
+                                    key={str}
+                                    arrow="horizontal"
+                                    multipleLine
+                                    onClick={() => { }}
+                                    thumb={img}
+                                >
+                                    <Link to={`${val.id}/detail`}>
+                                        宠物饲料 <Brief>{val.name}</Brief>
+                                    </Link>
+                                </Item>
+                            )
+                        }, this)
+                    }
+                </List>
+            </WingBlank>
+        );
     }
 }
 
