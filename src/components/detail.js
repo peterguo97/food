3502095d0db0cc2ab09c7af2805c3eeeb49562a3
gjj <1623144737@qq.com>
2018-04-22 11:45:54 +0800

    day7

--- src/components/detail.js
+++ src/components/detail.js
@@ -2,20 +2,19 @@ import React from 'react';
 import { List } from 'antd-mobile';
 import style from './css/detail.css';
 import img_title from '../assets/yay.jpg';
-import img_title1 from '../assets/food.png';
-
+import back from '../assets/back.png';
+import { Link } from 'dva/router';
 const Item = List.Item;
 const Brief = Item.Brief;
 
 class Detail extends React.Component {
     render(){
         return(
-            <List className={style.detail_head}>
-                <Item  
-                    style={{ background: `url(${img_title}) no-repeat`, filter:'alpha(Opacity=60)', opacity:'0.8'}}
+            <List className={style.detail_head} >
+                <Item
                     thumb={img_title}
                     multipleLine="true"
-                    extra={<div>hello world</div>}
+                    extra={<div><Link to="/"><img src={back} alt="back"/></Link></div>}
                 >
                     宠物狗饲料
                     <Brief>蜂鸟专送/极速送达</Brief>
